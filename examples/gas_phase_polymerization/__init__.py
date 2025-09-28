61f23168102d630f1b551358e6e311ea269cfd78
Aliuoly <122484271+Aliuoly@users.noreply.github.com>
2025-09-27 22:58:15 -0400

    Refactor gas-phase reactor example to framework style

---
+++ examples/gas_phase_polymerization/__init__.py
@@ -0,0 +1,17 @@
+"""Utilities for the gas-phase polymerization example system."""
+
+from .system_definitions import (
+    GasPhaseReactorAlgebraicStates,
+    GasPhaseReactorConstants,
+    GasPhaseReactorControlElements,
+    GasPhaseReactorStates,
+    GasPhaseReactorSystem,
+)
+
+__all__ = [
+    "GasPhaseReactorAlgebraicStates",
+    "GasPhaseReactorConstants",
+    "GasPhaseReactorControlElements",
+    "GasPhaseReactorStates",
+    "GasPhaseReactorSystem",
+]
